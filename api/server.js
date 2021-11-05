723bf9ccbb2ab0be5373b7f76c0002e6bfae41d7
Jaehun Park <pjaehun4122@gmail.com>
2021-11-05 11:23:29 -0700

    1passing dependecies installed

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+
+
+server.use('*', (req, res)=>{
+    res.json({api: 'up'})
+})
+
+module.exports=server
