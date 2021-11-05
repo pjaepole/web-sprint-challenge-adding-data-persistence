99ff37b24cc0064b7b23718a42baf2903898895a
Jaehun Park <pjaehun4122@gmail.com>
2021-11-05 12:03:20 -0700

    project table layout done

--- data/migrations/20211105184533_first-migration.js
+++ data/migrations/20211105184533_first-migration.js
@@ -3,6 +3,9 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('projects', table=>{
         table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed ').default('false')
     })
     .createTable('resources', table=>{
         table.increments('resource_id')
